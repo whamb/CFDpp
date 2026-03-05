f2e1840879639cf881e2e0739fc34d63c6d119a9
wmh25 <wmh17@ic.ac.uk>
2026-03-05 12:52:59 +0100

    Add chrono

--- Equation/SolveBurgers.cpp
+++ Equation/SolveBurgers.cpp
@@ -7,7 +7,6 @@ void SolveBurgers::cyclingStrategy(const Mesh& mesh, BurgersEqn& burgersEqn){
     burgersEqn.initialiseSolution(mesh);
     while(m_time < m_tFinal){
         advanceTime();
-        std::cout << "Time = " << m_time << "\n";
         m_tripletSystem.clear();
         burgersEqn.buildBurgers(mesh, m_tripletSystem);
         burgersEqn.u() = PetscCsrSolve::solveWithPETSc(mesh, CsrSystem(m_tripletSystem));
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,7 @@
+#include <chrono>
+#include <iostream>
+
+
 #include <BurgersEqn.hpp>
 #include <SolveBurgers.hpp>
 #include <TripletSystem.hpp>
@@ -8,7 +12,7 @@
 // A mock class to expose protected members for test validation (optional)
 
 int main(){
-    Double tFinal = 5.0;
+    Double tFinal = 100.0;
     Double dt = 0.05;
     Double nu = 0.1;
     Mesh mesh(0.0, 1, 0.01); 
@@ -16,8 +20,16 @@ int main(){
     BurgersEqn burgersEqn(mesh, dt, nu);
     SolveBurgers solveBurgers(mesh, tFinal, dt);
     PetscInitialize(NULL, NULL, NULL, NULL);
+
+    auto begin = std::chrono::steady_clock::now();
     solveBurgers.cyclingStrategy(mesh, burgersEqn);
+    auto end = std::chrono::steady_clock::now();
+
     PetscFinalize();
+
+     std::cout << "Elapsed milliseconds: "
+        << duration_cast<std::chrono::milliseconds>(end - begin).count()
+        << "ms\n";
     
     return 0;
 }
